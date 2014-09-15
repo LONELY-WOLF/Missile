3d1c7f8e19819f25905da151a6008a8ab23a57ea
LONELY-WOLF <zvenayte@pisem.net>
2014-09-15 20:39:05 +0400

    XML load added

--- Missile/Aircraft.cs
+++ Missile/Aircraft.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Media.Media3D;
+using System.Xml.Linq;
 
 namespace Missile
 {
@@ -14,10 +16,16 @@ namespace Missile
         Model3DGroup model;
         ObservableCollection<Part> parts = new ObservableCollection<Part>();
 
-        public Aircraft()
+        public Aircraft(string xmlFileName)
         {
+            XDocument doc = XDocument.Load(xmlFileName);
             model = new Model3DGroup();
-            Part p = new Part("1.STL");
+            FileInfo fi = new FileInfo(xmlFileName);
+
+            foreach(XElement part in doc.Root.Elements(XName.Get("Part")))
+            {
+                parts.Add(new Part(Path.Combine(fi.DirectoryName, part.Attribute(XName.Get("ModelFileName")).Value)));
+            }
         }
 
         #region INotifyPropertyChanged
